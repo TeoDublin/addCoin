3b892b2843adc173e52798e21fe6e3a03079d6db
TeoDublin <teodublin@gmail.com>
2025-01-06 12:44:52 +0100

    inp

--- pages/impostazioni/view.php
+++ pages/impostazioni/view.php
@@ -3,14 +3,11 @@
     <div class="flex-fill flex-column">
         <ul class="nav nav-tabs">
             <li class="nav-item">
-                <a class="nav-link <?php echo _tab('terapisti')?'active':'';?>" href="impostazioni.php?tab=terapisti&pagination=0" aria-current="page">Terapisti</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link <?php echo _tab('motivi')?'active':'';?>" href="impostazioni.php?tab=motivi&pagination=0" aria-current="page">Motivi</a>
+                <a class="nav-link <?php echo _tab('categorie')?'active':'';?>" href="impostazioni.php?tab=categorie&pagination=0" aria-current="page">Categorie</a>
             </li>
         </ul>
         <div class="p-1">
-            <?php $tab=cookie('tab','terapisti'); require "{$tab}/{$tab}.php";?>
+            <?php $tab=cookie('tab','categorie'); require "{$tab}/{$tab}.php";?>
         </div>
     </div>
 </div>
